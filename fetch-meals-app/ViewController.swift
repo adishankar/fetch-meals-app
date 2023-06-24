b53aa00d991e39a54788ad40116cfe656e1c0068
Adi Shankar <adi2895@gmail.com>
2023-06-23 23:27:39 -0400

    init app

--- fetch-meals-app/ViewController.swift
+++ fetch-meals-app/ViewController.swift
@@ -8,10 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var headerLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .white
+        
+        self.headerLabel = UILabel()
+        headerLabel.font = .boldSystemFont(ofSize: 36)
+        headerLabel.text = "Fetch Meals"
+        headerLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.view.addSubview(headerLabel)
+        let headerLeadingConstraint = NSLayoutConstraint(item: headerLabel, attribute: .leading, relatedBy: .equal, toItem: self.view.safeAreaLayoutGuide, attribute: .leading, multiplier: 1, constant: 20)
+        let headerTopConstraint = NSLayoutConstraint(item: headerLabel, attribute: .top, relatedBy: .equal, toItem: self.view.safeAreaLayoutGuide, attribute: .top, multiplier: 1, constant: 20)
+        self.view.addConstraints([headerLeadingConstraint, headerTopConstraint])
     }
 
 
