afb415baf956ab4faa751b1290b06e19ec6444e2
Adi Shankar <adi2895@gmail.com>
2023-06-27 23:00:28 -0400

    detail view sizing

--- fetch-meals-app/UI/MealDetail/Views/MealDetailView.swift
+++ fetch-meals-app/UI/MealDetail/Views/MealDetailView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct MealDetailView: View {
     
+    private var idiom : UIUserInterfaceIdiom { UIDevice.current.userInterfaceIdiom }
+    
     let mealDetailViewModel: MealDetailViewModel
     
     var body: some View {
@@ -22,8 +24,8 @@ struct MealDetailView: View {
                     } placeholder: {
                         Image(systemName: "fork.knife")
                     }
-                    .frame(maxWidth: .infinity, maxHeight: 200)
-                    .cornerRadius(0)
+                    .frame(maxWidth: .infinity, maxHeight: idiom == .phone ? 200 : 400)
+                    .cornerRadius(8)
                     Spacer()
                 }
                 HStack {
@@ -59,22 +61,28 @@ struct MealDetailView: View {
                 }
                 .padding(.top, 10)
                 VStack {
+                    Spacer().frame(height: 10)
                     ForEach(mealDetailViewModel.ingredients, id: \.ingredientName) { mealDetailIngredient in
                         HStack {
                             if let measurement = mealDetailIngredient.ingredientMeasurement {
                                 let ingredientText = "\(mealDetailIngredient.ingredientName), \(measurement)"
                                 Text(ingredientText)
                                     .font(.subheadline)
+                                    .padding(.leading, 10)
                             }
                             else {
                                 let ingredientText = "\(mealDetailIngredient.ingredientName)"
                                 Text(ingredientText)
                                     .font(.subheadline)
+                                    .padding(.leading, 10)
                             }
                             Spacer()
                         }
                     }
+                    Spacer().frame(height: 10)
                 }
+                .background(.yellow.opacity(0.1))
+                .cornerRadius(10)
                 HStack {
                     Text("Instructions")
                         .font(.title2)
